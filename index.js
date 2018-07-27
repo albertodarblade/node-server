a25e41760002711dd2730d5c99deb28aff941ded
Alberto Blacutt <alberto.darblade@gmail.com>
2018-07-27 03:36:52 -0400

    adding cors

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
-const app = express();
-
 const cors = require('cors');
 
-app.use(cors);
+const app = express();
+
+app.use(cors());
 app.get('/notes', function (req, res) {
   res.send([{title: 'himan', cost: '1000'}]);
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Localhost port 3000');
 });
